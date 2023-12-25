39433f3d340b5ee344923be46a8ab6f099052458
ianiani910425 <910425.ian@gmail.com>
2023-12-25 23:15:52 +0800

    斜線基礎指令

---
+++ src/commands/ping/index.js
@@ -0,0 +1,9 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export const command = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
+
+export const action = async (interaction) => {  
+    interaction.reply("Pong!");
+}   
